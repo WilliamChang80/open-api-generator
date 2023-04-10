4057a05c97978810d67904bb207cac310885fc79
williamchang80 <william_chang80@rocketmail.com>
2023-04-10 23:41:15 +0700

    Fix dir

--- script/publish-typescript.sh
+++ script/publish-typescript.sh
@@ -1,7 +1,6 @@
-cd ../generated/typescript
-echo "Current directory is: $(pwd)"
 npm install
 
 echo "npm.pkg.github.com/:_authToken=${NPM_CONFIG_TOKEN} " > .npmrc
 echo "@WilliamChang80:registry=https://npm.pkg.github.com always-auth=true" > .npmrc
+
 npm publish
