a53af30a41b83c58594f5cb6537204b3188dd590
williamchang80 <william_chang80@rocketmail.com>
2023-04-11 00:06:21 +0700

    Move dir

--- script/generate-typescript.sh
+++ script/generate-typescript.sh
@@ -1,6 +1,6 @@
 java -jar ./script/swagger-codegen-cli.jar generate \
     -i https://petstore.swagger.io/v2/swagger.json \
     -l typescript-axios \
-    -o ../generated/typescript
+    -o ./generated/typescript
 
-cp ./script/publish-typescript.sh ../generated/typescript
+cp ./script/publish-typescript.sh ./generated/typescript
