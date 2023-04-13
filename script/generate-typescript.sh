304325fb5a5d0e935b0e26ae74d601428b1c6850
williamchang80 <william_chang80@rocketmail.com>
2023-04-13 23:16:57 +0700

    Fix push tag

--- script/generate-typescript.sh
+++ script/generate-typescript.sh
@@ -1,6 +1,17 @@
+if [[ -z "$ARTIFACT_VERSION" ]]; then
+    echo "API version was not defined, terminating..."
+    exit 
+fi
+
+echo "Generating API with version $ARTIFACT_VERSION"
+
 java -jar ./script/swagger-codegen-cli.jar generate \
     -i https://petstore.swagger.io/v2/swagger.json \
     -l typescript-axios \
-    -o ./generated/typescript
+    -o ./generated/typescript \
+    --artifact-version $ARTIFACT_VERSION \
+    --additional-properties  \
+    npmName=@WilliamChang80/open-api, \
+    npmVersion=$ARTIFACT_VERSION
 
 cp ./script/publish-typescript.sh ./generated/typescript
--- script/publish-typescript.sh
+++ script/publish-typescript.sh
@@ -1,6 +1,8 @@
 npm install
 
 echo -e "@WilliamChang80:registry=https://npm.pkg.github.com" > .npmrc
-echo "//npm.pkg.github.com/:_authToken=${NPM_CONFIG_TOKEN} " >> .npmrc
+echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN} " >> .npmrc
 
 npm publish
+
+echo "Published package with version $ARTIFACT_VERSION"
