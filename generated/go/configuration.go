b48de56e2c6cd0343ef30cd01a480c794cc94e32
WilliamChang80 <WilliamChang80@users.noreply.github.com>
2023-08-03 16:22:06 +0000

    Generate go code version 984f29c

--- generated/go/configuration.go
+++ generated/go/configuration.go
@@ -56,7 +56,7 @@ func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		BasePath:      "/",
 		DefaultHeader: make(map[string]string),
-		UserAgent:     "Swagger-Codegen/6deef79/go",
+		UserAgent:     "Swagger-Codegen/984f29c/go",
 	}
 	return cfg
 }
