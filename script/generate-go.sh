d3775cecec58d17cc72cd215016eefd006cde937
williamchang80 <william_chang80@rocketmail.com>
2023-08-03 21:53:25 +0700

    feat: add go generator

---
+++ script/generate-go.sh
@@ -0,0 +1,15 @@
+if [[ -z "$ARTIFACT_VERSION" ]]; then
+    echo "API version was not defined, terminating..."
+    exit 
+fi
+
+echo "Generating API to $GENERATED_API_NAME with version $ARTIFACT_VERSION"
+
+java -jar ./script/swagger-codegen-cli.jar generate \
+    -i ./spec/swagger.yml \
+    -l go \
+    -o ./package/go \
+    --artifact-version $ARTIFACT_VERSION \
+    --additional-properties  \
+    packageName=$GENERATED_API_NAME, \
+    packageVersion=$ARTIFACT_VERSION
