23f90ab252f93b8eb89f52a96b725149de686816
williamchang80 <william_chang80@rocketmail.com>
2023-08-03 21:13:50 +0700

    fix: github repo variables

--- script/publish-typescript.sh
+++ script/publish-typescript.sh
@@ -1,6 +1,6 @@
 npm install
 
-echo -e "@${GITHUB_REPOSITORY_OWNER}:registry=https://npm.pkg.github.com" > .npmrc
+echo -e "@${github.repository_owner}:registry=https://npm.pkg.github.com" > .npmrc
 echo "//npm.pkg.github.com/:_authToken=${NPM_TOKEN} " >> .npmrc
 
 npm publish
